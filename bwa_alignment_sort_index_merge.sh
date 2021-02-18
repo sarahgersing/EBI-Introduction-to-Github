3e1d76f19a2709969a4e51138ac2d3ce6e311683
sarahgersing <sarah.gersing@bio.ku.dk>
2021-02-18 08:49:34 +0000

    fix script to index reference fasta before aligning

--- bwa_alignment_sort_index_merge.sh
+++ bwa_alignment_sort_index_merge.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-for n in {1..2}
-do
-	bwa mem -R '@RG\tID:'"$n"'\tLB:library\tPL:Illumina\tPU:lane'"$n"'\tSM:yeast' \
-	Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa \
-	lane$n/s-7-1.trim.paired.fastq lane$n/s-7-2.trim.paired.fastq | \
-	samtools view -b - | \
-	samtools sort - -o lane${n}_sorted.bam  &&
+samtools faidx Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa
 
-	samtools index lane${n}_sorted.bam
+bwa index -a is Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa
+
+picard CreateSequenceDictionary R=Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa O=Saccharomyces_cerevisiae.EF4.68.dna.toplevel.dict
+
+
+for n in 1 2
+do
+	bwa mem -R \
+	'@RG\tID:"'$n'"\tLB:library\tPL:Illumina\tPU:lane'"$n"'\tSM:yeast' \
+	Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa \
+	lane${n}/s-7-1.trim.paired.fastq lane${n}/s-7-2.trim.paired.fastq | \
+	samtools view -b - | \
+	samtools sort - -o lane${n}_sorted.bam  && samtools index lane${n}_sorted.bam
 done
 
 picard MergeSamFiles INPUT=lane1_sorted.bam INPUT=lane2_sorted.bam OUTPUT=library.bam
 
 samtools index library.bam
 
+samtools view -C -T Saccharomyces_cerevisiae.EF4.68.dna.toplevel.fa library.bam > library.cram
